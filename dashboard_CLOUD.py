e30b620fb9baa95705138d1c109533efe020e533
RAYMOND Olivier <34789438+Isdinval@users.noreply.github.com>
2024-06-06 21:05:01 +0200

    Update dashboard_CLOUD.py

--- dashboard_CLOUD.py
+++ dashboard_CLOUD.py
@@ -269,10 +269,11 @@ def main():
     # Remove DAYS EMPLOTYED ANOMALIES
     # Exclude the specific value from mean calculation (assuming it's unique)
     filtered_data = customer_data_copy[customer_data_copy['DAYS_EMPLOYED'] != 365243]
+    st.write(filtered_data)
     mean_days_employed = int(filtered_data['DAYS_EMPLOYED'].mean())
     st.write(mean_days_employed)
     customer_data_copy['DAYS_EMPLOYED']  = customer_data_copy['DAYS_EMPLOYED'].replace(365243, mean_days_employed, inplace=True)
-    st.write(customer_data_copy)
+    st.write(customer_data_copy['DAYS_EMPLOYED'])
 
 
     # =========================================================================
