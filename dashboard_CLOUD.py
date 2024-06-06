02ecc582f13875ec68a310ad781d8daa2bb3b29e
RAYMOND Olivier <34789438+Isdinval@users.noreply.github.com>
2024-06-06 15:57:46 +0200

    Update dashboard_CLOUD.py

--- dashboard_CLOUD.py
+++ dashboard_CLOUD.py
@@ -257,24 +257,24 @@ def main():
     # COMPARATIVE ANALYSIS USING GRAPHS 
     # =========================================================================
     # Get all features (assuming numerical features)
-    all_features = customer_data_preprocessed.select_dtypes(include=[np.number])  # Adjust for categorical features if needed
+    all_features = customer_data_copy.select_dtypes(include=[np.number])  # Adjust for categorical features if needed
     
     # Filter controls
     selected_feature = st.sidebar.selectbox('Select Feature:', all_features.columns)
     
     # Filter data based on selected feature
-    filtered_data = customer_data_preprocessed.copy()  # Avoid modifying original data
+    filtered_data = customer_data_copy.copy()  # Avoid modifying original data
     
     if selected_feature != 'All Clients':
       # Assuming a numerical feature is selected (adjust for categorical features)
-      filtered_data = filtered_data[filtered_data[selected_feature] <= customer_data_preprocessed[selected_feature].iloc[0]]
+      filtered_data = filtered_data[filtered_data[selected_feature] <= customer_data_copy[selected_feature].iloc[0]]
     
     
     # Generate graph
     st.header('Comparative Analysis')
     
     # Bar chart with highlighting
-    customer_value = customer_data_preprocessed[selected_feature].iloc[0]
+    customer_value = customer_data_copy[selected_feature].iloc[0]
     counts, bins = np.histogram(filtered_data[selected_feature])
     
     bar_colors = ['red' if val == customer_value else 'blue' for val in bins[:-1]]
