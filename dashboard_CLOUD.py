f1f9737eba11bfce7955eb4af93ab504170af61d
RAYMOND Olivier <34789438+Isdinval@users.noreply.github.com>
2024-06-06 15:27:44 +0200

    Update dashboard_CLOUD.py
    added the missing "}"

--- dashboard_CLOUD.py
+++ dashboard_CLOUD.py
@@ -300,7 +300,7 @@ def main():
         housing_type = input_data_copy['NAME_HOUSING_TYPE'].lower()
 
         customer_description = f"""
-        **{gender_pronoun.capitalize()}** is a **{age_years}** years old **{input_data_copy['NAME_EDUCATION_TYPE']}** who works in the **{input_data_copy['NAME_INCOME_TYPE']}** sector. **{gender_pronoun.capitalize()}** lives in a **{housing_type}** and is currently **{employment_duration_years}** years into employment. **{gender_pronoun.capitalize()}** is **{family_status}** and has applied for a **{input_data_copy['NAME_CONTRACT_TYPE'].lower()}** loan. **{gender_pronoun.capitalize()}** income is **{input_data_copy['AMT_INCOME_TOTAL']** €. 
+        **{gender_pronoun.capitalize()}** is a **{age_years}** years old **{input_data_copy['NAME_EDUCATION_TYPE']}** who works in the **{input_data_copy['NAME_INCOME_TYPE']}** sector. **{gender_pronoun.capitalize()}** lives in a **{housing_type}** and is currently **{employment_duration_years}** years into employment. **{gender_pronoun.capitalize()}** is **{family_status}** and has applied for a **{input_data_copy['NAME_CONTRACT_TYPE'].lower()}** loan. **{gender_pronoun.capitalize()}** income is **{input_data_copy['AMT_INCOME_TOTAL']}** €. 
         """
         st.write(customer_description)
         
