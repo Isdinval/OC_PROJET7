c95a43666893497ee73248bb877a94e71397862b
RAYMOND Olivier <34789438+Isdinval@users.noreply.github.com>
2024-06-06 16:33:09 +0200

    Update dashboard_CLOUD.py

--- dashboard_CLOUD.py
+++ dashboard_CLOUD.py
@@ -255,42 +255,6 @@ def main():
 
 
     # =========================================================================
-    # COMPARATIVE ANALYSIS USING GRAPHS
-    # =========================================================================
-    # Generate graph
-    st.header('Comparative Analysis')
-    
-    # Get all features (assuming numerical features)
-    all_features = customer_data_copy.select_dtypes(include=[np.number])  # Adjust for categorical features if needed
-    
-    # Filter controls
-    selected_feature = st.selectbox('Select Feature:', all_features.columns)
-    
-    # Filter data based on selected feature
-    filtered_data = customer_data_copy.copy()  # Avoid modifying original data
-    
-    # Separate data for full dataset and current customer
-    full_data_values = customer_data_copy[selected_feature]
-    customer_value = customer_data_copy[selected_feature].iloc[0]
-    st.write("full_data_values")
-    st.write(full_data_values)
-    st.write("customer_value")
-    st.write(customer_value)
-    
-    # Create line plot with highlighting
-    plt.plot(full_data_values, color='gray', label='All Clients', alpha=0.7)  # Add transparency for better visibility
-    
-    # Highlight current customer with red dot
-    plt.scatter(customer_value, customer_value, marker='o', color='red', s=100, label='Current Customer')  # Adjust marker size (s)
-    
-    plt.xlabel(selected_feature)
-    plt.ylabel('Value')
-    plt.title(f'{selected_feature} Distribution')
-    plt.legend()
-    plt.tight_layout()
-    st.pyplot(plt.gcf())
-
-    # =========================================================================
     # ADD Missing features Manually (due to preprocess) then re-order features
     # =========================================================================
 
@@ -320,7 +284,44 @@ def main():
         input_data      = customer_data_preprocessed[customer_data_preprocessed['SK_ID_CURR'] == sk_id_curr].iloc[0].to_dict()
         input_data_copy = customer_data_copy[customer_data_copy['SK_ID_CURR'] == sk_id_curr].iloc[0].to_dict()
         
+        # =========================================================================
+        # COMPARATIVE ANALYSIS USING GRAPHS
+        # =========================================================================
+        # Generate graph
+        st.header('Comparative Analysis')
+        
+        # Get all features (assuming numerical features)
+        all_features = customer_data_copy.select_dtypes(include=[np.number])  # Adjust for categorical features if needed
+        
+        # Filter controls
+        selected_feature = st.selectbox('Select Feature:', all_features.columns)
+        
+        # Filter data based on selected feature
+        filtered_data = customer_data_copy.copy()  # Avoid modifying original data
+        
+        # Separate data for full dataset and current customer
+        full_data_values = customer_data_copy[selected_feature]
+        customer_value = customer_data_copy[selected_feature].iloc[0]
+        st.write("full_data_values")
+        st.write(full_data_values)
+        st.write("customer_value")
+        st.write(customer_value)
+        
+        # Create line plot with highlighting
+        plt.plot(full_data_values, color='gray', label='All Clients', alpha=0.7)  # Add transparency for better visibility
+        
+        # Highlight current customer with red dot
+        plt.scatter(customer_index, customer_value, marker='o', color='red', s=100, label='Current Customer')  # Adjust marker size (s)
+        
+        plt.xlabel('Customer Index')
+        plt.ylabel(selected_feature)
+        plt.title(f'{selected_feature} Distribution')
+        plt.legend()
+        plt.tight_layout()
+        st.pyplot(plt.gcf())
 
+
+    
         # =========================================================================
         # CUSTOMERS BASIC INFORMATIONS
         # =========================================================================
