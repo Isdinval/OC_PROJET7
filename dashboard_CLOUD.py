2c9512c3fb025290774a15de0f012d747d2ae0b6
RAYMOND Olivier <34789438+Isdinval@users.noreply.github.com>
2024-06-06 22:31:24 +0200

    Update dashboard_CLOUD.py

--- dashboard_CLOUD.py
+++ dashboard_CLOUD.py
@@ -411,11 +411,11 @@ def main():
 
 
 
+        st.write(optimal_threshold * 100)
         streamviz.gauge(
-            probability_class1, gSize="LRG", gTitle=f"Probability of Default (Treshold: {optimal_threshold * 100:.2f}%)", sFix="%",
+            probability_class1, gSize="LRG", gTitle=f"Probability of Default (Treshold: "{optimal_threshold * 100:.2f}%), sFix="%",
             grLow=0, grMid=optimal_threshold * 100, gcLow="#1B8720", 
-            gcMid="#FF9400", gcHigh="#FF1708")
-
+            gcMid="#1B8720", gcHigh="#FF1708")
 
 
 
