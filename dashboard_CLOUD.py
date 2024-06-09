5640abec502020854bd5fdafe8f7f65652f27344
RAYMOND Olivier <34789438+Isdinval@users.noreply.github.com>
2024-06-09 09:33:15 +0200

    Update dashboard_CLOUD.py

--- dashboard_CLOUD.py
+++ dashboard_CLOUD.py
@@ -362,7 +362,7 @@ def main():
         feature_description = feature_descriptions[feature_descriptions["Row"] == selected_feature]["Description"].iloc[0]
         
         # Print description
-        st.write(f"Description de la feature : **{feature_description}**")
+        st.write(f"Feature : **{feature_description}**")
 
 
 
@@ -413,9 +413,9 @@ def main():
         feature2 = st.selectbox('Select Feature 2:', all_features.columns, index=all_features.columns.get_loc('AMT_ANNUITY'))
 
         feature1_description = feature_descriptions[feature_descriptions["Row"] == feature1]["Description"].iloc[0]         # Find description for the selected feature
-        st.write(f"Description de la feature 1 : **{feature1_description}**")
+        st.write(f"Feature 1 : **{feature1_description}**")
         feature2_description = feature_descriptions[feature_descriptions["Row"] == feature2]["Description"].iloc[0]         # Find description for the selected feature
-        st.write(f"Description de la feature 2 : **{feature2_description}**")
+        st.write(f"Feature 2 : **{feature2_description}**")
 
 
 
